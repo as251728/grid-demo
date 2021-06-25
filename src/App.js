ce32601b526dddafe7db56742046200070f3d9fc
Stewart <as251728@wusas251728-zpd.attlocal.net>
2021-06-25 08:39:03 -0400

    first pass

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+import { Grid, Typography, Button } from '@ncr-design-system/react';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='master-grid'>
+        <div className='nav'>
+        </div>
+        <Grid container className='content-grid' spacing={2}>
+          <Grid item xs={1}>
+            <div className='column-content'>
+            </div>
+          </Grid>
+          <Grid item xs={1}>
+            <div className='column-content'>
+            </div>
+          </Grid>
+          <Grid item xs={1}>
+            <div className='column-content'>
+            </div>
+          </Grid>
+          <Grid item xs={1}>
+            <div className='column-content'>
+            </div>
+          </Grid>
+          <Grid item xs={1}>
+            <div className='column-content'>
+            </div>
+          </Grid>
+          <Grid item xs={1}>
+            <div className='column-content'>
+            </div>
+          </Grid>
+          <Grid item xs={1}>
+            <div className='column-content'>
+            </div>
+          </Grid>
+          <Grid item xs={1}>
+            <div className='column-content'>
+            </div>
+          </Grid>
+          <Grid item xs={1}>
+            <div className='column-content'>
+            </div>
+          </Grid>
+          <Grid item xs={1}>
+            <div className='column-content'>
+            </div>
+          </Grid>
+          <Grid item xs={1}>
+            <div className='column-content'>
+            </div>
+          </Grid>
+          <Grid item xs={1}>
+            <div className='column-content'>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+}
+
+export default App;
